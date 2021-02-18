175df030efb6936de62b244bf32e4e4457b5624c
Rassska <rasul.yunusov282750@gmail.com>
2021-02-18 16:37:32 +0300

    added some comments

--- newBackUpModel/src/main.cpp
+++ newBackUpModel/src/main.cpp
@@ -20,16 +20,16 @@ int main() {
         BackUpModel* currModel = new BackUpModel();
         BackUp* simpleBackUp = new BackUp();
 
-        simpleBackUp->addFilesToBackUp(std::make_pair(std::string_view ("kek"), false));
+        simpleBackUp->addFilesToBackUp(std::make_pair(std::string_view ("kek"), false)); // second arg defines that file isn't in some Restore Point (this file is new)
         simpleBackUp->addFilesToBackUp(std::make_pair(std::string_view ("lol"), false));
         simpleBackUp->addFilesToBackUp(std::make_pair(std::string_view ("tik"), false));
         simpleBackUp->addFilesToBackUp(std::make_pair(std::string_view ("tok"), false));
         
-        simpleBackUp->createRestPoint(/* isIncrement ==*/ false);
+        simpleBackUp->createRestPoint(false); // increment or not
 
         simpleBackUp->addFilesToBackUp(std::make_pair(std::string_view ("mem"), false));
 
-        simpleBackUp->createRestPoint(/* isIncrement ==*/ false);
+        simpleBackUp->createRestPoint(false); // increment or not
 
         SaveManager* currManager = new SaveManager();
         ISave* currSaveAlg = new SaveJoin();
